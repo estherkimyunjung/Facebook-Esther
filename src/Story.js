fa94aa5afebe463487d7636955b2f21f4cdc177e
estherkimyunjung <estherkimyunjung@gmail.com>
2021-06-07 18:00:37 -0500

    add link for feed

--- src/Story.js
+++ src/Story.js
@@ -6,6 +6,7 @@ function Story({ image, profileSrc, title }) {
   return (
     <div className="story" style={{ backgroundImage: `url(${image})` }}>
       <Avatar className="story__avatar" src={profileSrc} />
+
       <h4>{title}</h4>
     </div>
   );
--- src/StoryReal.js
+++ src/StoryReal.js
@@ -5,26 +5,42 @@ import Story from "./Story";
 function StoryReal() {
   return (
     <div className="storyReal">
-      <Story
-        image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/154595136_10159306322968033_5074848652494213880_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=z6doNKhJIsMAX_it07m&tn=5-I3oUs1bFWhCvHE&_nc_ht=scontent-hou1-1.xx&oh=1f8ed4b8eed2938d150560a6d9b96160&oe=60E4380A"
-        profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/97978132_10219020026950441_6194945872950722560_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=MDHY3XXfNvwAX8Dp-ge&tn=5-I3oUs1bFWhCvHE&_nc_ht=scontent-hou1-1.xx&oh=ef48fa510806aaa23a8bfea19b4dbaaa&oe=60E1C229"
-        title="Esther Kim"
-      />
-      <Story
-        image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/173370511_10159334603844461_2161317258902749903_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=e3f864&_nc_ohc=s6XgX3ZBdn0AX_KNne8&_nc_ht=scontent-hou1-1.xx&oh=74bd36ddf76b66bd09204e821638c920&oe=60E3853C"
-        profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/133177172_10159050603684461_480211688264287295_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=uWckYpIVj-sAX-quYMv&tn=5-I3oUs1bFWhCvHE&_nc_ht=scontent-hou1-1.xx&oh=d8bfaafd3d6ededcd3c6d4912640dfc6&oe=60E34B66"
-        title="Jenny Kim"
-      />
-      <Story
-        image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/81513974_10206882478297136_4329509527528407040_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pYelqONZYIUAX-fAOa8&_nc_ht=scontent-hou1-1.xx&oh=8f321cb813a8845c487b237d13e5b594&oe=60E1908F"
-        profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-0/c105.0.414.414a/p552x414/32267055_10204617731959893_5300601469945249792_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=da31f3&_nc_ohc=g7LHwgvrEawAX9lSTK1&_nc_ht=scontent-hou1-1.xx&tp=27&oh=57e7c837a9b4468e532254a288b07c0a&oe=60E2FC1F"
-        title="Mary Bergman"
-      />
-      <Story
-        image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/139014994_10221675039659072_5476873628177677511_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=e3f864&_nc_ohc=04nFW6CTOZAAX8CkI3k&_nc_ht=scontent-hou1-1.xx&oh=e9054fbb38f4f297135a3d547b14dd97&oe=60E52D93"
-        profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/115905925_10220059595153969_4508135824939088930_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=UmaGO20HTGoAX8_2Iko&_nc_ht=scontent-hou1-1.xx&oh=6df173970422eb2855dfeb32fcbe196e&oe=60E1CEE4"
-        title="Gale Kot Lemer"
-      />
+      <a href="https://www.facebook.com/esther.kim.984991" target="_blank">
+        <Story
+          image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/154595136_10159306322968033_5074848652494213880_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=z6doNKhJIsMAX_it07m&tn=5-I3oUs1bFWhCvHE&_nc_ht=scontent-hou1-1.xx&oh=1f8ed4b8eed2938d150560a6d9b96160&oe=60E4380A"
+          profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/97978132_10219020026950441_6194945872950722560_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=MDHY3XXfNvwAX8Dp-ge&tn=5-I3oUs1bFWhCvHE&_nc_ht=scontent-hou1-1.xx&oh=ef48fa510806aaa23a8bfea19b4dbaaa&oe=60E1C229"
+          title="Esther Kim"
+        />
+      </a>
+
+      <a href="https://www.facebook.com/jenny.kim.9022662" target="_blank">
+        <Story
+          image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/173370511_10159334603844461_2161317258902749903_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=e3f864&_nc_ohc=s6XgX3ZBdn0AX_KNne8&_nc_ht=scontent-hou1-1.xx&oh=74bd36ddf76b66bd09204e821638c920&oe=60E3853C"
+          profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/133177172_10159050603684461_480211688264287295_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=uWckYpIVj-sAX-quYMv&tn=5-I3oUs1bFWhCvHE&_nc_ht=scontent-hou1-1.xx&oh=d8bfaafd3d6ededcd3c6d4912640dfc6&oe=60E34B66"
+          title="Jenny Kim"
+        />
+      </a>
+      <a href="https://www.facebook.com/mary.bergman.5" target="_blank">
+        <Story
+          image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/81513974_10206882478297136_4329509527528407040_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pYelqONZYIUAX-fAOa8&_nc_ht=scontent-hou1-1.xx&oh=8f321cb813a8845c487b237d13e5b594&oe=60E1908F"
+          profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-0/c105.0.414.414a/p552x414/32267055_10204617731959893_5300601469945249792_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=da31f3&_nc_ohc=g7LHwgvrEawAX9lSTK1&_nc_ht=scontent-hou1-1.xx&tp=27&oh=57e7c837a9b4468e532254a288b07c0a&oe=60E2FC1F"
+          title="Mary Bergman"
+        />
+      </a>
+      <a href="https://www.facebook.com/gale.kot" target="_blank">
+        <Story
+          image="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/139014994_10221675039659072_5476873628177677511_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=e3f864&_nc_ohc=04nFW6CTOZAAX8CkI3k&_nc_ht=scontent-hou1-1.xx&oh=e9054fbb38f4f297135a3d547b14dd97&oe=60E52D93"
+          profileSrc="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/115905925_10220059595153969_4508135824939088930_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=UmaGO20HTGoAX8_2Iko&_nc_ht=scontent-hou1-1.xx&oh=6df173970422eb2855dfeb32fcbe196e&oe=60E1CEE4"
+          title="Gale Kot Lemer"
+        />
+      </a>
+      <a href="https://www.facebook.com/jae.kim.54943" target="_blank">
+        <Story
+          image="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.18169-9/20882900_10155661200703033_2106453763231185889_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=e3f864&_nc_ohc=2qi7dRXu8iMAX-sME4j&_nc_ht=scontent-dfw5-1.xx&oh=e77e5e4036b894c6a3210fa12d8e24ee&oe=60E4E637"
+          profileSrc="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/100060018_10158511375803033_1581189491308101632_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=EvTApV6nzWAAX-3_7Zf&_nc_ht=scontent-dfw5-2.xx&oh=d83e90d3f9b7a0c5feef7933463b68eb&oe=60E41A4D"
+          title="Jae Kim"
+        />
+      </a>
     </div>
   );
 }
